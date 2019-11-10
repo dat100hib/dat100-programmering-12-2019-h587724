9704b2dee59e42a12dcd51874b97509c010cc340
Arsenii Dmitriev <54366538+h587724@users.noreply.github.com>
2019-11-10 20:27:29 +0100

    Complete 1-4

--- src/no/hvl/dat100/jplab12/oppgave3/Blogg.java
+++ src/no/hvl/dat100/jplab12/oppgave3/Blogg.java
@@ -1,62 +1,116 @@
 package no.hvl.dat100.jplab12.oppgave3;
 
+//import java.util.ArrayList;
+
 import no.hvl.dat100.jplab12.common.TODO;
 import no.hvl.dat100.jplab12.oppgave1.*;
 
 public class Blogg {
 
-	// TODO: objektvariable 
-
+	// TODO: objektvariable
+	protected Innlegg[] blogg2; //claimed the array
+	protected int nextvar;
+	
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
-	}
+		this.blogg2 = new Innlegg[20]; //constructor for the declaration
+	}								
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		this.blogg2 = new Innlegg[lengde];
+		this.nextvar = 0;
 	}
 
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		return nextvar; 
 	}
 	
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		
+		return blogg2;
 	}
 	
 	public int finnInnlegg(Innlegg innlegg) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		int position = -1;
+		int id1 = innlegg.getId();
+		for (int i = 0; i<blogg2.length; i++) {
+			if (id1 == blogg2[i].getId()) {
+				position = i;
+			}
+		}
+		return position;
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		for (int i = 0; i<blogg2.length; i++) {
+			if (blogg2[i] != null) {
+				if (innlegg.erLik(blogg2[i]) == true) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		
+		boolean exists = false;
+		for (int i = 0; i < blogg2.length; i++) {
+			if (blogg2[i] == null) {
+				exists = true;
+			}
+		}
+		return exists;
 	}
 	
 	public boolean leggTil(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		
+		boolean done;
+		if (finnes(innlegg) == false) {
+			blogg2[nextvar] = innlegg;
+			nextvar ++;
+			done = true;
+		} else { done = false; }
+		return done;
 	}
 	
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		String output = nextvar + "\n";
+		for (int i = 0; i < blogg2.length; i++) {
+			if (finnes(blogg2[i]) == true) {
+		output += blogg2[i].toString();
+			}
+		}
+		return output;
 	}
 
 	// valgfrie oppgaver nedenfor
 	
 	public void utvid() {
-		throw new UnsupportedOperationException(TODO.method());
+		Innlegg [] expandedblogg = new Innlegg [blogg2.length * 2];
+		for (int i = 0; i < blogg2.length; i++) {
+			expandedblogg[i] = blogg2[i];
+		}
+		blogg2 = expandedblogg;
 	}
 	
 	public boolean leggTilUtvid(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
 		
+		boolean accomplished = true;
+		for (int i = 0; i < blogg2.length; i++) {
+			if (innlegg.erLik(blogg2[i]) == true) {
+				accomplished = false;
+			}
+		}
+			if (nextvar > blogg2.length) {
+				utvid();
+			}
+			if (accomplished) {
+				blogg2[nextvar] = innlegg;
+				return accomplished;
+			}
+		return false;
 	}
 	
 	public boolean slett(Innlegg innlegg) {
--- src/no/hvl/dat100/jplab12/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab12/oppgave4/SkrivBlogg.java
@@ -3,8 +3,7 @@ package no.hvl.dat100.jplab12.oppgave4;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
-import no.hvl.dat100.jplab12.oppgave3.*;
-import no.hvl.dat100.jplab12.common.TODO;
+import no.hvl.dat100.jplab12.oppgave3.Blogg;
 
 public class SkrivBlogg {
 
@@ -12,6 +11,15 @@ public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		try {
+			PrintWriter pw = new PrintWriter (MAPPE + filnavn);
+			pw.print(samling.toString());
+			pw.close();
+			return true;
+		}
+		catch (FileNotFoundException e) {
+			System.out.print("Error: " + e);
+			return false;
+		}
 	}
 }
